d9dc70cc96cc2fc6d3e46b1e6eaf974bc3e52f95
Amit Sharma <asharm36@hawk.iit.edu>
2017-03-21 22:23:25 -0500

    kangaroo.py

--- Implementation/kangaroo.py
+++ Implementation/kangaroo.py
@@ -2,19 +2,19 @@ import sys
 
 def kangaroo(x1,x2,v1,v2):
     while True:
-    if x1>x2 :
-        if v1 >= v2:
-            print("NO")
+        if x1>x2 :
+            if v1 >= v2:
+                print("NO")
+                break
+        elif x2>x1:
+            if v2>=v1:
+                print("NO")
+                break
+        elif x1 == x2:
+            print("YES")
             break
-    elif x2>x1:
-        if v2>=v1:
-            print("NO")
-            break
-    elif x1 == x2:
-        print("YES")
-        break
-    x1 += v1
-    x2 += v2
+        x1 += v1
+        x2 += v2
 
 if __name__ == "__main__":
     x1,v1,x2,v2 = input().strip().split(' ')
