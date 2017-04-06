f9f71d53dd361a9b0de537f11ce06410add61a26
Amit Sharma <asharm36@hawk.iit.edu>
2017-04-05 20:22:28 -0500

    MagicSquareForming.py

---
+++ Implementation/MagicSquareForming.py
@@ -0,0 +1,35 @@
+def msf(perm,matrix,mincost):
+
+    for itm in perm:
+        curr = 0
+        for i in range(3):
+            for j in range(3):
+                curr += abs(matrix[i][j] - itm[i][j])
+
+        mincost = min(curr, mincost)
+    print (mincost)
+
+
+def main():
+    perm = [[[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+        [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+        [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+        [[2, 9, 4], [7, 5, 3], [6, 1, 8]],
+        [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+        [[4, 3, 8], [9, 5, 1], [2, 7, 6]],
+        [[6, 7, 2], [1, 5, 9], [8, 3, 4]],
+        [[2, 7, 6], [9, 5, 1], [4, 3, 8]]]
+
+    matrix = []
+
+    for i in range(3):
+        matrix.append(list(map(int, input().strip().split(' '))))
+
+    mincost = 81
+
+    msf(perm,matrix,mincost)
+
+
+
+if __name__ == '__main__':
+    main()
